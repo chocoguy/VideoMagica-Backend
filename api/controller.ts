66747679ee2818a0a4cb6d47cced5defe73dde1f
Edgar Zarco <chocoguy@mailfence.com>
2024-06-26 22:17:21 -0500

    No progress

--- api/controller.ts
+++ api/controller.ts
@@ -1,14 +1,20 @@
 import express, { Express, Request, Response, Application } from 'express';
-import { Innertube } from 'youtubei.js';
+import { Innertube, UniversalCache } from 'youtubei.js';
 import { VideoInfo } from 'youtubei.js/dist/src/parser/youtube';
 
 //MKBHD top 5 android 15 features:  kkX8_nbBqBQ
+
+//req.body.whatever
 
 class controller {
     public static async testRoute(req: Request, res: Response){
         try{
             const youtube = await Innertube.create({
-                retrieve_player: true
+                retrieve_player: true,
+                cache: new UniversalCache(
+                    true,
+                    './.cache'
+                )
             });
 
             console.log("Getting video");
@@ -31,6 +37,48 @@ class controller {
             return res.status(500).json({"error" : error})
         }
     }
+
+    public static async getVideoFull(req: Request, res: Response){
+
+    }
+
+    public static async getVideoLite(req: Request, res: Response){
+
+    }
+
+    public static async searchContent(req: Request, res: Response){
+
+    }
+
+    public static async getChannel(req: Request, res: Response){
+        try{
+            let youtube = await Innertube.create({
+                retrieve_player: false,
+                cache: new UniversalCache(
+                    true,
+                    './.cache'
+                )
+            });
+
+            let channel = await youtube.getChannel(req.body.channel)
+            
+
+        }
+        catch(error: any){
+            console.error(`Error on getChannel ${error}`)
+            return res.status(500).json({"error" : error})
+        }
+    }
+
+    public static async getVideoComments(req: Request, res: Response){
+
+    }
+
+    public static async videoDownload(req: Request, res: Response){
+
+    }
+
+
 }
 
 export default controller
